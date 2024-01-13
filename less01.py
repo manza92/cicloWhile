ffa1f4c0e16c96308c51f700d8b5df90490f862f
Darwin Manzano <manza_92@yahoo.com>
2024-01-12 18:49:16 -0600

    ciclo while en python

---
+++ less01.py
@@ -0,0 +1,17 @@
+# ciclo while
+
+condicion = True
+
+while condicion:
+    print('Ejecutando ciclo while')
+else:
+    print('Fin del ciclo while')
+
+minimo = 1
+contador = 5
+
+while contador >= minimo:
+    print(contador)
+    contador -= 1
+else:
+    print('Fin ciclo while')
